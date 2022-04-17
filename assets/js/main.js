eb0487a10af1c0c81e8e4b6d566fa238e24ae2b9
alidjanoff <mrdeathly007@gmail.com>
2022-04-18 00:18:34 +0400

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+let toggle = document.querySelector('.toggle');
+let menu = document.querySelector('.menu');
+
+/* -------------------------- START Click function -------------------------- */
+toggle.onclick = function(){
+    menu.classList.toggle('active')
+}
+/* --------------------------- END Click function --------------------------- */
